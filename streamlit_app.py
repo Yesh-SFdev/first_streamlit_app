111399123722c3c639e3849f3b321d5b82d438a2
Yeshwanth Yadav <114341204+Yesh-SFdev@users.noreply.github.com>
2022-09-25 18:12:52 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('SnowFlake Developer')
+streamlit.header('Yeshwanth_Yadav')
+streamlit.text('Started his career on SnowFlake')
+streamlit.text('He started working on SnowFalke')
+streamlit.text('He was performing great')
+streamlit.text('But his grades were not up to the mark')
+streamlit.text('He never gave up and he stoodup and gave his best')
