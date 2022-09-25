972c3900b9ec3b47fe0fbe31ed86cd198d3326b0
Yeshwanth Yadav <114341204+Yesh-SFdev@users.noreply.github.com>
2022-09-25 18:17:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit
 
 streamlit.title('SnowFlake Developer')
-streamlit.header('Yeshwanth_Yadav')
-streamlit.text('Started his career on SnowFlake')
-streamlit.text('He started working on SnowFalke')
-streamlit.text('He was performing great')
-streamlit.text('But his grades were not up to the mark')
-streamlit.text('He never gave up and he stoodup and gave his best')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
